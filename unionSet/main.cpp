ab7c7a3654b4347759c38ff58fbbd02eb0d679d9
Pas0quit0 <31050100+Pas0quit0@users.noreply.github.com>
2019-08-07 10:49:33 -0300

    union set

--- unionSet/main.cpp
+++ unionSet/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>>
 
 using namespace std;
 
+class union_find {
+    vector<int> p, rank;
+    public:
+        int q;
+        union_find(int n){
+            q = n;
+            rank.assign(n, 0);
+            p.assign(n, 0);
+            for(int i = 0; i < n; i++) p[i] = i;
+        }
+
+        int find_set(int i){
+            return (p[i] == i) ? i : (p[i] = find_set(p[i]));
+        }
+
+        bool is_same_set(int i, int j){
+            return find_set(i) == find_set(j);
+        }
+
+        void union_set(int i, int j){
+            if(!is_same_set(i,j)){
+                int x = find_set(i), y = find_set(j);
+                if(rank[x] > rank[y]) p[y] = x;
+                else{
+                    p[x] = y;
+                    if (rank[x] == rank[y]) rank[y]++;
+                }
+                q--;
+            }
+        }
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
     return 0;
 }
